54e558689b87114f30c766d6a146bcb0517d61a9
Ivan Saavedra <ivan.saavedra.iezs@gmail.com>
2023-02-20 22:24:39 -0600

    demostacion del block scope

---
+++ scope/block.js
@@ -0,0 +1,27 @@
+// function fruits() {
+//     if(true) {
+//         var fruta1 = "pinia";
+//         let fruta2 = "manzana";
+//         const fruta3 = "platano";
+//     }
+
+//     console.log(fruta1);
+//     console.log(fruta2);
+//     console.log(fruta3);
+// }
+
+// fruits();
+
+function fruits() {
+    if(true) {
+        var fruta1 = "pinia";
+        let fruta2 = "manzana";
+        const fruta3 = "platano";
+        console.log(fruta2);
+        console.log(fruta3);
+    }
+
+    console.log(fruta1);
+}
+
+fruits();
