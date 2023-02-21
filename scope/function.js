68cfb12bee84c854a0ae6908560556d200e6d9a1
Ivan Saavedra <ivan.saavedra.iezs@gmail.com>
2023-02-20 22:14:13 -0600

    vimos el funcionamiento del function scope

---
+++ scope/function.js
@@ -0,0 +1,12 @@
+function greeting() {
+    let userName = "Pablo";
+    console.log(userName);
+
+    if(userName === 'Pablo'){
+        console.log(`Hello ${userName}!`);
+    }
+}
+
+greeting();
+
+console.log(userName);
