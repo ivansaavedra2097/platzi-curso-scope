4536f0da50a3587cd60ba1a5624c33230d8e472d
Ivan Saavedra <ivan.saavedra.iezs@gmail.com>
2023-02-20 22:42:23 -0600

    trabajamos con asignacion, declaracion, reasignacion y redeclaracion con var, let y const

---
+++ scope/asig_decl_reasig.js
@@ -0,0 +1,36 @@
+//declaracion
+var fistName; //valor = undefined
+//asignacion
+firstName = "Oscar";
+console.log(firstName);
+
+//declaracion-asignacion
+var lastName = "David";
+//redeclarar
+lastName = "Perez";
+console.log(lastName);
+
+//delcarar - asignar
+var secondName = "David";
+//redeclarando - reasignando
+var secondName = "Anastasio";
+console.log(secondName);
+
+//let 
+let fruit = "Apple";
+fruit = "Uva";
+console.log(friut);
+
+let fruit = "Platano";
+
+//const 
+const animal = "perro";
+animal = "gato";
+
+console.log(animal);
+
+const arr = [];
+arr.push('🚬');
+console.log(arr);
+arr.pop();
+console.log(arr);
