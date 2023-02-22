2fe13d8aea1f058c7643d89c01d57e70aed8ca6f
Ivan Saavedra <ivan.saavedra.iezs@gmail.com>
2023-02-21 21:29:14 -0600

    aplicamos el concepto de Hoisting

---
+++ hoisting/index.js
@@ -0,0 +1,13 @@
+// var nameDog = 'Froxi';
+// console.log(nameDog);
+// var nameDog = 'Kika';
+// console.log(nameDog);
+
+
+function nameDog() {
+    console.log(`Perrito precioso ${perro}`);
+}
+var perro = 'foxy';
+nameDog();
+
+console.log(nameDog);
